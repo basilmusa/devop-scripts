c456815a3b891588128978fcf533a03103c4e615
Basil Musa <basilmusa@users.noreply.github.com>
2020-06-15 16:52:31 +0400

    Update postgresql-mount-data-dir.sh
    In progress

--- mount-data-drive/postgresql-mount-data-dir.sh
+++ mount-data-drive/postgresql-mount-data-dir.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 # THIS DEPENDS ON THE PREVIOUS SCRIPT AND BOTH COULD BE MERGED
 set -eux
-apt-get update
-apt-get install -y postgresql
+
+# This script assumes
+# 1. A directory is mounted on a new location called NEW_DATA_DIR
+# 2. CURRENT_DATA_DIR holds the current PostgreSQL 
+NEW_DATA_DIR=$1
+POSTGRES_DATA_DIR=/var/lib/postgresql/9.5/main
+
+# Check NEW_DATA_DIR does not end with "/" character, if so trim it
+# Check NEW_DATA_DIR is a directory
+# Check NEW_DATA_DIR is already mounted in /etc/fstab
+# Check NEW_DATA_DIR is not the same as POSTGRES_DATA_DIR
+
+# First time do this without shutting down postgresql
+rsync -aHAX ${NEW_DATA_DIR}/* ${POSTGRES_DATA_DIR}
+
 systemctl stop postgresql
-umount /data || /bin/true
-mv /var/lib/postgresql /var/lib/postgresql.bak
-mkdir -p /var/lib/postgresql
-sed  -i 's/ \/data / \/var\/lib\/postgresql /g' /etc/fstab
-mount -a
-rsync -aHAX /var/lib/postgresql.bak/* /var/lib/postgresql
-chown -R postgres:postgres /var/lib/postgresql
-rm -rf "/var/lib/postgresql.bak"
-rm -rf "/var/lib/postgresql/lost+found"
+
+rsync -aHAX ${NEW_DATA_DIR}/* ${POSTGRES_DATA_DIR}
+chown -R postgres:postgres ${NEW_DATA_DIR}
+
 systemctl start postgresql
